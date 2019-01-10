e1b9a5f3b33bbd56a6ccbb8751de82a5cb8eaa4e
manisiimaiyah00 <iamtsmani@gmail.com>
2019-01-10 19:26:19 +0530

    test.php is changed 1

--- test.php
+++ test.php
@@ -1,1 +1,41 @@
-hijiojioiojfiojdiofjiodjfodjfj
+hijiojioiojfiojdio
+Filming
+Principal photography began in June 2018. The first schedule of the shoot began on 7 June at Eastern Forest Rangers College at St. Mary’s Hill, Kurseong in Darjeeling, with the initial portions lasting nine days. The team then shot in other location across Darjeeling and the hill towns for a further month, in locations including St. Paul’s and Mount Hermon School.[43][44] To finish the first schedule, the team returned to Kurseong for six days. An audition to cast 150 local actors was also held in Charbagh, Lucknow during the first schedule.[45] During the making of the schedule, Rajinikanth met with politician Gautam Deb, who helped the makers have a hassle free shoot in exchange for the promotion of tourism in the area.[46] Principal photography wrapped in October 2018, fifteen days ahead of schedule.[47]
+
+Promotion
+On 25 December Sun Pictures announced 28 December as the trailer release date of the film. Taran Adarsh tweeted.[48] Launching new look poster of the film Sun Pictures official handler on tweeter gave the trailer release time on 28 December.[49]
+
+Release
+Petta was released on 10 January 2019.[50]
+
+Soundtrack
+Petta (Original Motion Picture Soundtrack)
+Soundtrack album by Anirudh Ravichander
+Released        9 December 2018
+Recorded        2018
+Studio
+Albuquerque Records, Chennai
+Tapas Studios, Chennai
+Knack Studios, Chennai
+Genre   Feature film soundtrack
+Length  27:43
+Language        Tamil
+Label   Sony Music
+Producer        Anirudh Ravichander
+Kalanithi Maran
+Anirudh Ravichander chronology
+Kolamavu Kokila
+(2018)  Petta (Original Motion Picture Soundtrack)
+(2018)  Jersey
+(2019)
+Singles from Petta
+"Marana Mass"
+Released: 3 December 2018
+"Ullallaa"
+Released: 7 December 2018
+The soundtrack was composed by Anirudh Ravichander, replacing Subbaraj's usual music director Santhosh Narayanan who collaborated for his films before.[51]
+
+The first single, a dappan koothu number titled "Marana Mass" was released on December 3, featuring the vocals of Anirudh Ravichander and S. P. Balasubrahmanyam, where the latter marks his return to sing Rajini's intro song after a gap of 4 years. The second single and the full album tracklist are expected to be released on December 7 and December 9 respectively.[52][53]
+
+The Audio Rights was bagged under Sony Music in 6th December 2018
+fjiodjfodjfj
